d62750e019d43dee2ec0b7adb2dab4e8b006ddc4
daffiprogs <urlaubkcs@gmx.de>
2016-06-02 18:05:25 +0200

    Wir haben die Hintergrundfarbe für Button 1 auf Rot geändert, um uns im Editor die verschiedenen Möglichkeiten für die Eigenschaft Farbe anzuschauen.

--- panel/Form1.Designer.vb
+++ panel/Form1.Designer.vb
@@ -22,9 +22,77 @@ Partial Class Form1
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.panMove = New System.Windows.Forms.Panel()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Red
+        Me.Button1.Location = New System.Drawing.Point(105, 26)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(197, 110)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(105, 207)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(12, 110)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Button4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'panMove
+        '
+        Me.panMove.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.panMove.Location = New System.Drawing.Point(93, 83)
+        Me.panMove.Name = "panMove"
+        Me.panMove.Size = New System.Drawing.Size(98, 95)
+        Me.panMove.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.panMove)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents panMove As System.Windows.Forms.Panel
 End Class
--- panel/Form1.vb
+++ panel/Form1.vb
@@ -1,3 +1,23 @@
 ﻿Public Class Form1
+    Private Sub NachOben(sender As Object, e As EventArgs) Handles Button1.MouseHover
+        panMove.Location = New Point(panMove.Location.X,
+                                     panMove.Location.Y - 10)
+    End Sub
 
+    Private Sub NachRechts(sender As Object, e As EventArgs) Handles Button2.Click
+        panMove.Location = New Point(
+           panMove.Location.X + 10,
+           panMove.Location.Y)
+    End Sub
+
+    Private Sub NachUnten(sender As Object, e As EventArgs) Handles Button3.Click
+        panMove.Location = New Point(
+            panMove.Location.X,
+            panMove.Location.Y + 10)
+    End Sub
+
+    Private Sub NachLinks(sender As Object, e As EventArgs) Handles Button4.Click
+        panMove.Location = New Point(panMove.Location.X - 10,
+            panMove.Location.Y)
+    End Sub
 End Class
